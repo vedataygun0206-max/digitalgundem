af52339ea3bd21c726292a26ede1bcb224582b51
digitalgundem <vedataygun0206@gmail.com>
2026-08-02 00:19:58 +0300

    Add fetch function to retrieve RSS feed

--- worker.js
+++ worker.js
@@ -1,1 +1,16 @@
+export default {
+  async fetch(request) {
+    const rss = "https://www.trthaber.com/sondakika_articles.rss";
 
+    const response = await fetch(
+      "https://api.allorigins.win/raw?url=" + encodeURIComponent(rss)
+    );
+
+    return new Response(await response.text(), {
+      headers: {
+        "Content-Type": "application/xml",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+  }
+}
